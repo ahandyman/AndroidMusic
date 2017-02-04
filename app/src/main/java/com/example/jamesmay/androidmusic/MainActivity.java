0954ef56afd598171ff117c8522ab48882b074c3
ahandyman <ramonvzcrr@gmail.com>
2017-02-04 14:13:56 -0800

    Added descriptions to each activity

--- app/src/main/java/com/example/jamesmay/androidmusic/MainActivity.java
+++ app/src/main/java/com/example/jamesmay/androidmusic/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.jamesmay.androidmusic;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
@@ -43,5 +43,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(changeActivity_favorites);
             }
         });
+
+
     }
 }
